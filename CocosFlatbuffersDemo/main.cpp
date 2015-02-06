efc6ed67cb525f2829dc3f04cf490f481e166367
Richie Yan <richie.yan@outlook.com>
2015-02-06 14:52:02 +0800

    Initial Commit

---
+++ CocosFlatbuffersDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CocosFlatbuffersDemo
+//
+//  Created by Richie Yan on 2/6/15.
+//  Copyright (c) 2015 Richie Yan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
